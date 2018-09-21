4238b1fb8333757bd6e672664e12ddb3ea6e4e15
olgabot <olga.botvinnik@gmail.com>
2018-09-20 19:32:29 -0700

    add tab padding

--- check_reflow_status.py
+++ check_reflow_status.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 reflow_dirs = '/home/olga/code/kmer-hashing/sourmash/compare_no_track_abundance', '/home/olga/code/kmer-hashing/sourmash/compare', '/home/olga/code/tick-genome/pre_assembly_qc/full_workflow', '/home/olga/code/kmer-hashing/sourmash/search', '/home/olga/code/kmer-hashing/sourmash/lung_cancer_v4/search_tabula-muris-k51-protein/'
 
+statuses = 'done', 'running', 'waiting', 'canceled'
+
 for reflow_dir in reflow_dirs:
     print(reflow_dir)
     listbatch = get_ipython().getoutput('cd $reflow_dir && reflow listbatch')
@@ -19,6 +21,6 @@ for reflow_dir in reflow_dirs:
 
     print(f'\tOther statuses:')
     for other, n in n_others.items():
-        print(f'\t{other}: {n}/{n_total}')
+        print(f'\t\t{other}: {n}/{n_total}')
 
             
