85a2f49608debff8d64100156a33203174c5e619
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-02-14 07:59:47 -0800

    Use log2 num hashes

--- scripts/sourmash_compare_numhashes.py
+++ scripts/sourmash_compare_numhashes.py
@@ -2,10 +2,11 @@ import glob
 import itertools
 import os
 import sys
+import tempfile
 import time
 
 import click
-from joblib import Parallel, delayed
+from joblib import Parallel, delayed, load, dump
 import numpy as np
 import pandas as pd
 from scipy.spatial.distance import squareform
@@ -15,7 +16,7 @@ from tqdm import tqdm
 
 KSIZES = 21, 27, 33, 51
 MOLECULES = 'dna', 'protein'
-NUM_HASHES = 500, 1000, 2000, 5000, 10000, 20000, 50000, 10000
+LOG2_NUM_HASHES = 8, 9, 10, 11, 12, 13, 14, 15, 16
 
 
 def filter_siglist(siglist, ksize, moltype):
@@ -52,28 +53,44 @@ def _compare_serial(siglist, iterator):
 
 def compare_all_pairs(siglist, n_jobs=None):
     n = len(siglist)
-    iterator = itertools.combinations(range(n), 2)
 
-    if n_jobs is None:
+    # Combinations makes all unique sets of pairs, e.g. (A, B) but not (B, A)
+    iterator = itertools.combinations(range(n), 2)
+    sig_iterator = itertools.combinations(siglist, 2)
+
+    if n_jobs is None or n_jobs == 1:
         values = _compare_serial(siglist, iterator)
     else:
         # This creates a condensed distance matrix
-        condensed = Parallel(n_jobs=n_jobs)(
-            delayed(jaccard_utils.jaccard_sigs)(i, j, siglist) for i, j in
-            iterator)
+        condensed = Parallel(n_jobs=n_jobs, require='sharedmem')(
+            delayed(sig1.jaccard)(sig2) for sig1, sig2 in sig_iterator)
         values = squareform(condensed)
 
     return values
 
 
-def downsample_and_compare(signatures, num_hash, molecule, ksize):
+def _memmap_siglist(siglist):
+    """Write a memory-mapped array of signatures"""
+    temp_folder = tempfile.mkdtemp()
+    filename = os.path.join(temp_folder, 'joblib_test.mmap')
+    if os.path.exists(filename): os.unlink(filename)
+    _ = dump(siglist, filename)
+    large_memmap = load(filename, mmap_mode='r+')
+    return large_memmap
+
+
+
+def downsample_and_compare(signatures, log2_num_hash, molecule, ksize):
     t0 = time.time()
     filtered = filter_siglist(signatures, ksize, molecule)
     t1 = time.time()
+    num_hash = 2**log2_num_hash
     downsampled = [s.minhash.downsample_n(num_hash) for s in filtered]
     t2 = time.time()
-    values = compare_all_pairs(downsampled)
+    memmapped = _memmap_siglist(downsampled)
     t3 = time.time()
+    values = compare_all_pairs(memmapped)
+    t4 = time.time()
 
     names = [s.name().split('|')[0] for s in downsampled]
 
@@ -83,16 +100,17 @@ def downsample_and_compare(signatures, num_hash, molecule, ksize):
           f"ks§ize: {ksize} ---")
     print(f"Time to filter on ksize and molecule: {t1-t0:.2}s")
     print(f"Time to downsample on num_hash: {t2-t1:.2}s")
-    print(f"Time to compare all pairs: {t3-t2:.2}s")
+    print(f"Time to write memory-mapped array: {t3-t2:.2}s")
+    print(f"Time to compare all pairs: {t4-t3:.2}s")
     return df
 
 
 
 @click.command()
 @click.argument("filenames", nargs=-1)
-@click.option("--num-hashes",
-              help=f"Which hash values to compare. Default: {NUM_HASHES}",
-              default=NUM_HASHES)
+@click.option("--log2-num-hashes",
+              help=f"Which hash values to compare. Default: {LOG2_NUM_HASHES}",
+              default=LOG2_NUM_HASHES)
 @click.option("--molecules",
               help=f"Which hash values to compare. Default: {MOLECULES}",
               default=MOLECULES)
@@ -104,21 +122,23 @@ def downsample_and_compare(signatures, num_hash, molecule, ksize):
               default=".")
 @click.option("--n-jobs", help=f"Number of processes to use",
               default=8)
-def cli(filenames, num_hashes=NUM_HASHES, molecules=MOLECULES, ksizes=KSIZES,
+def cli(filenames, log2_num_hashes=LOG2_NUM_HASHES, molecules=MOLECULES, ksizes=KSIZES,
         outdir=".", n_jobs=8):
     click.echo(f"Loading {len(filenames)} signature files ...")
     signatures = load_signatures(filenames)
 
-    iterable = itertools.product(num_hashes, molecules, ksizes)
+    iterable = itertools.product(log2_num_hashes, molecules, ksizes)
 
     similarities = Parallel(n_jobs=n_jobs)(
-        delayed(downsample_and_compare)(signatures, num_hash, molecule, ksize)
-        for num_hash, molecule, ksize in iterable)
+        delayed(downsample_and_compare)(signatures, log2_num_hash,
+                                        molecule, ksize)
+        for log2_num_hashes, molecule, ksize in iterable)
 
-    iterable = itertools.product(num_hashes, molecules, ksizes)
+    iterable = itertools.product(log2_num_hashes, molecules, ksizes)
 
-    for (num_hash, molecule, ksize), df in zip(iterable, similarities):
-        filename = f"molecule-{molecule}_ksize-{ksize}_numhash-{num_hash}.csv"
+    for (log2_num_hash, molecule, ksize), df in zip(iterable, similarities):
+        filename = f"molecule-{molecule}_ksize-{ksize}_" \
+                    "log2numhash-{log2_num_hash}.csv"
         csv = os.path.join(outdir, filename)
         click.echo(f"Writing {csv} ...")
         df.to_csv(csv)
